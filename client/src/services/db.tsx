736a39741ec4ef77039e420ec92ed9f6151e4e71
dzalee16 <mladen.jovic93@yahoo.com>
2021-04-01 18:27:04 +0200

    Create services for geting data from db

---
+++ client/src/services/db.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { Difficulty } from "../API/index";
+
+const server = "http://localhost:5000";
+
+//get users
+export const getUsers = async () => {
+  return axios.get(`${server}/users`);
+};
+
+//create users
+type data = {
+  username: string;
+  score: number;
+  time: number;
+  difficulty: Difficulty;
+};
+
+export const createUser = async (data: data) => {
+  return axios.post(`${server}/users`, data);
+};
